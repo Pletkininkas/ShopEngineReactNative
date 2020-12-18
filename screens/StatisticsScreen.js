6333594d9824d990d688ee8b807f4736c14e34ba
Gintarebalsyte <gintarebalsyte00@gmail.com>
2020-12-18 07:14:19 +0200

    slight change in design

--- screens/StatisticsScreen.js
+++ screens/StatisticsScreen.js
@@ -6,13 +6,61 @@ import {
     BarChart
   } from "react-native-chart-kit";
 import moment from 'moment';
-import styles from '../config/styles';
+import configStyles from '../config/styles';
 import configColors from '../config/colors';
+import config, { user } from '../config';
+import { color } from 'react-native-reanimated';
 
 const StatisticsScreen = ({navigation}) => {
 
     const {colors} = useTheme();
     const [months, setMonths] = useState(['']);
+    //const[graphData, setGraphData] = useState([0]);
+    //const [receipts, setReceipts] = useState([]);
+    
+      // useState(() => {
+      //   let token = user.token;
+      //   fetch(config.API_URL+'receipt', {
+      //     method: 'GET',
+      //     headers: {
+      //       Accept: 'application/json',
+      //       'Content-Type': 'application/json',
+      //       'Authorization': 'Bearer ' + token
+      //     }
+      //   }).then(data => {
+      //       return data.json();
+      //     })
+      //     .then(data => {
+      //       setReceipts(data.data);
+      //     })
+      //     .catch(err => {
+      //       console.log(err);
+      //     });
+  
+      // }, []);
+
+    //   function getGraphData() {
+    //     var data = [];
+    //     data.length = 12; 
+    //     data .fill(0);
+    //     receipts.forEach(element => {
+    //     var date = new Date(element.date);
+    //     var year = date.getFullYear();
+    //     var month = date.getMonth();
+
+    //     var currentDate = moment().add(1, 'days');
+    //     var startDate = moment().subtract(11, 'months');
+    //     var i;
+    //     for (i = 0; startDate < currentDate; i++, startDate.add(1, 'months')) {
+    //       if (year === startDate.year() && month === (startDate.month()+1))
+    //       {
+    //         data[i] +=  element.total;
+    //         break;
+    //       }
+    //     }
+    //   })
+    //  return data;
+    // }
     
     function getListOfAllMonths() {
         var currentDate = moment();
@@ -31,6 +79,7 @@ const StatisticsScreen = ({navigation}) => {
       // Subscribe for the focus Listener
       const unsubscribe = navigation.addListener('focus', () => {
         setMonths(getListOfAllMonths());
+        //setGraphData(getGraphData());
       });
       return unsubscribe;
     }, [navigation]);
@@ -40,20 +89,25 @@ const StatisticsScreen = ({navigation}) => {
         datasets: [
           {
             data: [20, 45]
+            //data: graphData 
           }
         ]
       };
 
     const chartConfig = {
-        backgroundGradientFrom: configColors.green,
-        backgroundGradientTo: colors.background,
+      backgroundGradientFrom: "#1E2923",
+      backgroundGradientFromOpacity: 0,
+      //backgroundGradientTo: "#08130D",
+      backgroundGradientToOpacity: 0.5,
+        // backgroundGradientFrom: colors.background,
+      backgroundGradientTo: configColors.green,
         color: (opacity = 1) => colors.text,
         barPercentage: 2,
         useShadowColorFromDataset: false, 
         data: barData.datasets,
         decimalPlaces: 2, 
         labelColor: (opacity = 1) => colors.text,
-        fillShadowGradient:'skyblue',
+        fillShadowGradient:configColors.green,
         fillShadowGradientOpacity:1,
         style: {
             borderRadius: 16
@@ -61,18 +115,16 @@ const StatisticsScreen = ({navigation}) => {
       };
 
     return (
-      <View style={styles().containerm}>
-          <View>
-                <Text style={styles().title}>Statistics</Text>
-          </View>
-          <View style={styles().bodym}>
-          <Text style={{fontSize: 20, color: colors.text, textAlign: 'center'}}>Year Spendings Pattern</Text> 
+      <View style={configStyles().container} backgroundColor={colors.background}>
+          <View style={configStyles().bodym} justifyContent={'space-evenly'}>
+          <Text style={{fontSize: 30, color: colors.text, textAlign: 'center'}}>Year Spendings Pattern</Text> 
           <LineChart
             data={{
                 labels: months,
                 datasets: [
                     {
                         data: [20, 45, 28, 80, 99, 43, 20, 45, 28, 80, 99, 43]
+                        //data: graphData 
                     }
                 ]
                 }}
@@ -85,7 +137,7 @@ const StatisticsScreen = ({navigation}) => {
                 chartConfig={chartConfig}
                 />
 
-                <Text style={{fontSize: 20, color: colors.text, textAlign: 'center'}}>Average Monthly Spendings</Text>
+                <Text style={{fontSize: 30, color: colors.text, textAlign: 'center'}}>Average Monthly Spendings</Text>
 
                 <BarChart
                     //style={graphStyle}
